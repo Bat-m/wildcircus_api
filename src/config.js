8dded7dbe69144ff29b2ed78b3f8855712d2c1ce
Baptiste <53108208+BaptisteMonet@users.noreply.github.com>
2020-02-12 19:51:27 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: process.env.HOST_WILDCIRCUS,
   user: process.env.USER_WILDCIRCUS,
   password: process.env.PWD_WILDCIRCUS,
